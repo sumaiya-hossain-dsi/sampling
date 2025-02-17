5fd90eb597b2895f790ef31c4390322874f6f7a0
Sumaiya <maiyahossain@gmail.com>
2025-02-16 21:11:04 -0500

    whitby-100

--- 02_activities/assignments/whitby_covid_tracing.py
+++ 02_activities/assignments/whitby_covid_tracing.py
@@ -68,7 +68,7 @@ def simulate_event(m):
   return p_wedding_infections, p_wedding_traces
 
 # Run the simulation 1000 times
-results = [simulate_event(m) for m in range(1000)]
+results = [simulate_event(m) for m in range(100)]
 props_df = pd.DataFrame(results, columns=["Infections", "Traces"])
 
 # Plotting the results
